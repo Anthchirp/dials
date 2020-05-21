c6de9dd55dc0c60d0c5195e5c72164577bd335ea
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-05-21 17:19:45 +0100

    fix test

--- test/test_scitbx.py
+++ test/test_scitbx.py
@@ -3,10 +3,6 @@ from __future__ import absolute_import, division, print_function
 from scitbx.array_family import flex
 
 
-def test_usd():
-    assert hasattr(isinstance, "show"), "Not an USD python"
-
-
 def test_complex_double_none_comparison():
     """For unknown reasons this can return a flex.bool() object on some builds."""
     assert (flex.complex_double() == None) is False  # noqa:E711
